cc70a86476680e6afd1f68a010f76f5fe0f59e88
Svetlozar Draganov <svetlozardraganov@gmail.com>
2024-03-02 11:47:18 +0200

    Add port

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 
 const addonInterface = require("./addon");
-serveHTTP(addonInterface, { port: 7000 });
+serveHTTP(addonInterface,  { port: process.env.PORT || 7000 });
+
+console.log(`Started addon at: http://localhost:${process.env.PORT || 7001}/manifest.json`);
+
 
